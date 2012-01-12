51f7c5ab6978435f96abded2769c5e6d1eab91e8
Joe Ferner <joe@fernsroth.com>
2012-01-12 17:16:29 -0500

    shell of hello world

---
+++ examples/helloWorld.js
@@ -0,0 +1,47 @@
+
+var wxNode = require("../");
+
+var ID_Quit = 1;
+var ID_About = 2;
+
+var MyApp = wxNode.wxApp.extend({
+  onInit: function() {
+    var frame = new MyFrame("Hello World", new wxNode.wxPoint(50, 50), new wxNode.wxSize(450, 340));
+    frame.show(true);
+    this.setTopWindow(frame);
+    return true;
+  }
+});
+
+var MyFrame = wxNode.wxFrame.extend({
+  init: function(title, pos, size) {
+    this._super(null, -1, title, pos, size);
+
+    var menuFile = new wxNode.wxMenu();
+    menuFile.append(ID_About, "&About");
+    menuFile.appendSeparator();
+    menuFile.append(ID_Quit, "E&xit");
+
+    var menuBar = new wxNode.wxMenuBar();
+    menuBar.append(menuFile, "&File");
+
+    this.setMenuBar(menuBar);
+
+    this.createStatusBar();
+    this.setStatusText("Welcome to wxNode!");
+
+    this.EVT_MENU(ID_About, this.onAbout);
+    this.EVT_MENU(ID_Quit, this.onQuit);
+  },
+
+  onQuit: function(event) {
+    this.close(true);
+  },
+
+  onAbout: function(event) {
+    wxNode.wxMessageBox("This is a wxNode Hello world sample", "About Hello World", wxNode.wxOK | wxNode.wxICON_INFORMATION, this);
+  }
+});
+
+var app = new MyApp();
+app.run();
